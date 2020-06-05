ee877b358b7ebd41b21ae302d43834ff4f6f1054
andanao <47489123+andanao@users.noreply.github.com>
2020-06-05 16:50:24 -0400

    file cleanup

--- 03 Time Tracking/clockify_test.ahk
+++
@@ -1,8 +0,0 @@
-key = "XtQRoKauzTS8sYKC"
-
-oWhr := ComObjCreate("WinHttp.WinHttpRequest.5.1")
-oWhr.Open("GET", "/workspaces/{workspaceId}/clients", false)
-oWhr.SetRequestHeader("Content-Type", "application/json")
-oWhr.SetRequestHeader("Authorization", "Bearer 80b44ea9c302237f9178a137d9e86deb-20083fb12d9579469f24afa80816066b")
-oWhr.Send()
-MsgBox, % oWhr.ResponseText
--- 03 Time Tracking/toggl_test.py
+++ 03 Time Tracking/toggl_test.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from toggl.TogglPy import Toggl as togglpy#god i love python
 
 class Toggl(togglpy):
-    toggl = Toggl()
+    toggl = togglpy()
     token = "089c874aefeb3e6a4d655c73819949be"
     toggl.setAPIKey(token)
     Projects = {}
@@ -17,11 +17,8 @@ class Toggl(togglpy):
     def stopRunningEntry(self):
         self.stopTimeEntry(self.currentRunningTimeEntry()['data']['id'])
 
-
-
     def getAllProjects(self):
         response = self.request("https://www.toggl.com/api/v8/clients")
-        
         for client in response:
             # print("Client name: %s  Client id: %s" % (client['name'], client['id']))
             temp = self.getClientProjects(id=client['id'])
@@ -30,10 +27,16 @@ class Toggl(togglpy):
                 # print("%s \t%s \t%s" % (proj['name'],client['name'],proj['id']))
 
     def startEntryFromProjectsList(self,input,Shortcuts):
-        self.startTimeEntry("test desc",self.Projects[input]["id"])
+        self.startTimeEntry("Testing Toggl Launcher",self.Projects[input]["id"])
         print("Time entry started for:\n %s" % (input))
+
+
+    def undoRecentTime(self,time):
+        proj = self.currentRunningTimeEntry()['data']
+        pprint.pprint(proj,width =2)
+        # toggl.createTimeEntry()
 
 
 
 toggl = Toggl()
-pprint.pprint(toggl.Projects,width =2)
+# pprint.pprint(toggl.Projects,width =2)
