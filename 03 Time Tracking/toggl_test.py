a6e92a19aa5f1ecbc423332fd4d2fbed098bf61c
andanao <47489123+andanao@users.noreply.github.com>
2020-06-05 16:06:55 -0400

    got start from list working

--- 03 Time Tracking/toggl_test.py
+++ 03 Time Tracking/toggl_test.py
@@ -13,18 +13,26 @@ toggl.setAPIKey(token)
 def stopRunningEntry():
     toggl.stopTimeEntry(toggl.currentRunningTimeEntry()['data']['id'])
 
-response = toggl.request("https://www.toggl.com/api/v8/clients")
-
-# print the client name and id for each client in the response
-# list of returned values can be found in the Toggl docs:
-# https://github.com/toggl/toggl_api_docs/blob/master/chapters/clients.md
 
 
-Projects = {}
-for client in response:
-    # print("Client name: %s  Client id: %s" % (client['name'], client['id']))
-    temp = toggl.getClientProjects(id=client['id'])
-    for proj in temp:
-        Projects[proj['name']] = proj
-        # print("%s \t%s \t%s" % (proj['name'],client['name'],proj['id']))
+
+
+def getAllProjects():
+    response = toggl.request("https://www.toggl.com/api/v8/clients")
+    Projects = {}
+    for client in response:
+        # print("Client name: %s  Client id: %s" % (client['name'], client['id']))
+        temp = toggl.getClientProjects(id=client['id'])
+        for proj in temp:
+            Projects[proj['name']] = proj
+            # print("%s \t%s \t%s" % (proj['name'],client['name'],proj['id']))
+    return Projects
+
+
+def progress():
+    toggl.startTimeEntry("Toggl Tool Progress Fn",161116831)
+
+def startEntryFromProjectsList(input,Projects,Shortcuts):
+    toggl.startTimeEntry("test desc",Projects[input]["id"])
+    print("%s started \nNot Complete" % (input))
 
