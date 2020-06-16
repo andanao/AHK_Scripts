162238448c5331ff1be5171f09e9cf70a289f941
andanao <47489123+andanao@users.noreply.github.com>
2020-06-16 16:35:16 -0400

    gui looks roughly ok

--- 03 Time Tracking/toggl_gui.py
+++ 03 Time Tracking/toggl_gui.py
@@ -6,14 +6,14 @@ def exit(event):
 
 def next_cell(event):
     if not html_link.get():
-        link_entry.focus()
+        user_entry.focus()
     elif html_link.get() and not file_name.get():
         file_entry.focus()
     else:
         convert()
-        link_entry.delete('0',END)
+        user_entry.delete('0',END)
         file_entry.delete('0',END)
-        link_entry.focus()
+        user_entry.focus()
 
 def convert():
     print2gui('\n\n---\tConverting from UCIS to AILA\t---\n')
@@ -52,30 +52,19 @@ root.rowconfigure(0, weight=1)
 html_link = StringVar()
 file_name = StringVar()
 
-link_entry = ttk.Entry(mainframe,width=70, textvariable=html_link, font='consolas')
-link_entry.grid(column = 2, row=1, columnspan = 3)
+user_entry = ttk.Entry(mainframe,width=60, textvariable=html_link, font='consolas')
+user_entry.grid(column = 1, row=1, columnspan = 3)
 
-file_entry = ttk.Entry(mainframe, width = 70, textvariable=file_name, font='consolas')
-file_entry.grid(column = 2, row=2, columnspan = 3)
+refresh_button = ttk.Button(mainframe, text = u'\u27F3',command=convert, width=10)
+refresh_button.grid(column=4,row=1, columnspan=2,sticky=E)
 
-file_dir_default = IntVar(value=1)
-file_dir_select = ttk.Checkbutton(mainframe, text='Use Default Directory',variable=file_dir_default)
-file_dir_select.grid(column=1,row=3, columnspan=2, sticky=W)
-
-# add text to all areas where necessary
-ttk.Label(mainframe,text ="Hyperlink").grid(column=1, row=1, sticky=W)
-ttk.Label(mainframe,text ="File Name").grid(column=1, row=2, sticky=W)
-
-convert_button = ttk.Button(mainframe, text = 'Convert UCIS',command=convert, width=50)
-convert_button.grid(column=3,row=3, columnspan=2,sticky=E)
-
-text_box = Text(mainframe,width=80,height=8, font='consolas',state='disabled', background='black',fg='white')
+text_box = Text(mainframe,width=70,height=8, font='consolas',state='disabled', background='black',fg='white')
 text_box.grid(column=1,row=4,columnspan=4)
 
 # pad 5px in all directions
 for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
 
-link_entry.focus()
+user_entry.focus()
 
 #Define keyboard shortcuts while in window
 root.bind("<Escape>", exit)
