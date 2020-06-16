87e430730dad126eac5d7289f093036220f1b0ae
andanao <47489123+andanao@users.noreply.github.com>
2020-06-16 17:07:11 -0400

    moved code in from toggl test

--- 03 Time Tracking/toggl_gui.py
+++ 03 Time Tracking/toggl_gui.py
@@ -1,6 +1,58 @@
 from tkinter import *
 from tkinter import ttk, filedialog
-from toggl_test import Toggl
+from json import load
+from toggl.TogglPy import Toggl as togglpy#god i love python
+from toggl.TogglPy import Endpoints
+from difflib import get_close_matches
+
+class Toggl(togglpy):
+    Projects = {}
+    def __init__(self,APIKey):
+        self.setAPIKey(APIKey)
+        # self.getAllProjects()\
+        self.shortcuts_dir='shortcuts.json'
+        self.loadShortcutsFile()
+
+    def loadShortcutsFile(self):
+        try:
+            with open(self.shortcuts_dir, encoding ='utf-8') as f:
+                self.shortcuts = load(f)
+        except NotADirectoryError:
+            print("file not found")
+
+    def stopRunningEntry(self):
+        self.stopTimeEntry(self.currentRunningTimeEntry()['data']['id'])
+
+    def getAllProjects(self):
+        response = self.request("https://www.toggl.com/api/v8/clients")
+        for client in response:
+            # print("Client name: %s  Client id: %s" % (client['name'], client['id']))
+            temp = self.getClientProjects(id=client['id'])
+            for proj in temp:
+                self.Projects[proj['name']] = proj
+                # print("%s \t%s \t%s" % (proj['name'],client['name'],proj['id']))
+
+    def useShortcut(self,input):
+        try:
+            name =  get_close_matches(input,self.shortcuts,n=1)[0]
+        except:
+            print('No entry found')
+
+        entry = self.shortcuts[name]
+        # print(entry)
+        
+        if entry['command'] == 'start_entry':
+            self.startTimeEntry(entry['desc'],entry['id'])
+            print('Starting entry:\n\t',entry['desc'])
+
+        elif entry['command'] == 'ask_entry':
+            desc = name #entry['desc']# + input("Enter Description:")
+            self.startTimeEntry(desc,entry['id'])
+            print('Starting entry:\n\t',desc)
+
+        elif entry['command'] == 'stop_entry':
+            self.stopRunningEntry()
+
 
 class GUI:
     def __init__(self,token):
@@ -37,15 +89,14 @@ class GUI:
 
         #Define keyboard shortcuts while in window
         self.root.bind("<Escape>", self.exit)
-        # self.root.bind('<Return>',self.next_cell)
+        self.root.bind('<Return>',self.run_shortcut)
         self.root.mainloop()
     
     def exit(self, event):
         self.root.destroy()
         
-
     def refresh(self):
-        self.print2gui('\n\n---\Refreshing Shortcuts File\t---\n')
+        self.print2gui('\n---\tRefreshing Shortcuts File\t---\n')
         self.toggl.loadShortcutsFile()
 
     def print2gui(self, string):
@@ -54,6 +105,9 @@ class GUI:
         self.text_box.see(END)
         self.text_box.configure(state="disabled")
     
+    def run_shortcut(self,event):
+        self.toggl.useShortcut(self.entry_string)
+
     
 token = "089c874aefeb3e6a4d655c73819949be"
 gui = GUI(token)
