89b93e506932e94713c26935e8a98d0b9ebd27d9
andanao <47489123+andanao@users.noreply.github.com>
2020-11-05 15:56:32 -0500

    added monte_carlo_test

---
+++ 04 Finances/health_care.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as numpy
+import seaborn as sns
+
+class health_plan:
+    def __init__(self,name,ppc):
+        self.name = name
+        self.period_cost = ppc
+        
+
+    def yearly_total(self):
+        pass
+
+    def run_sim(self):
+        pass
