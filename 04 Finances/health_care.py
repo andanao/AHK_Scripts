4d978b62f5f1941c651962aff20d8eb288024a50
andanao <47489123+andanao@users.noreply.github.com>
2020-11-05 19:30:13 -0500

    being lazy and using for loops ¯\_(ツ)_/¯ it doesn't have to be fast

--- 04 Finances/health_care.py
+++ 04 Finances/health_care.py
@@ -1,20 +1,21 @@
 import pandas as pd
-import numpy as numpy
+import numpy as np
 import seaborn as sns
 
 plans_df = pd.read_excel('health_plans.xlsx',header=[0,1],index_col=0)
 
 # 
-sim_runs = 1000
+sim_runs = 1
 
-prob_er = .5
-prob_uc = .2
-prob_i_surg = .2
-prob_o_surg= .1
+#        ER     UC      in_op   out_op
+probs = np.matrix([.5,     .2,      .05,   .25])
+costs = np.matrix([1000,  100,    10000,  5000])
 
-cost_er = 1000
-cost_uc = 100
-cost_i_surg = 10000
-cost_o_surg = 5000
+#allright gonna be lazy about this for loops it is
 
-# 
+for i in range(sim_runs):
+    rand = np.random.normal(loc = 1)
+    np.multiply(probs,stat).round(0)
+
+
+
