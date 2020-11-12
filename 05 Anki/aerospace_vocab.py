e761503843a0ebefc0a26da7b31e2e5c93105885
andanao <47489123+andanao@users.noreply.github.com>
2020-11-11 20:36:49 -0500

    regex working/1

--- 05 Anki/aerospace_vocab.py
+++ 05 Anki/aerospace_vocab.py
@@ -19,8 +19,21 @@ for item in find1:
     cleaned_text += item.text
 
 cleaned_text = cleaned_text[5:]
-cleaned_text = re.sub('[\s,\d]','',cleaned_text)
+cleaned_text = re.sub(r'[\s,\d]','',cleaned_text)
+cleaned_text = re.sub(r'([a-zA-Z]{1})(?=[\u4e00-\u9fff]{1})','\\1,\n',cleaned_text)
+cleaned_text = re.sub(r'([\u4e00-\u9fff]{1})(?=[a-zA-Z]{1})','\\1,',cleaned_text)
+# cleaned_text = re.sub(r'[\s,\d]','',cleaned_text)
 
-hanzi = re.findall(r'[\u4e00-\u9fff]+',cleaned_text)
-words = re.findall(r'(?<=[\u4e00-\u9fff]{1})[a-zA-Z]+?(?=[\u4e00-\u9fff]+)',cleaned_text)
 
+
+hanzi = re.findall(r'[\u4e00-\u9fff]+(?=,)',cleaned_text)
+words = re.findall(r'(?<=,)[a-zA-Z]+',cleaned_text)
+
+ 
+print(len(hanzi))
+print(len(words))
+
+for i in range(len(hanzi)):
+    
+    print(hanzi[i]+'\t'+words[i])
+    
