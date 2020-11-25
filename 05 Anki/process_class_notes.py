e28cc825624de4506ad8eb9404d2f1963061ae3d
andanao <47489123+andanao@users.noreply.github.com>
2020-11-25 11:30:10 -0500

    clean_notes working

--- 05 Anki/process_class_notes.py
+++ 05 Anki/process_class_notes.py
@@ -2,7 +2,28 @@ import pinyin
 import anki_tools
 import pandas as pd
 import re
+import click
+
+def clean_notes():
+    file = open("class_notes.txt", "r",encoding='utf-8').read()
+
+    cleaned_text = re.sub(r'(?<=[\u4e00-\u9fff])\d','',file)
+    cleaned_text = re.sub(r'.*\:.*','',cleaned_text)
+    cleaned_text = re.sub(r'.*\ds.*','',cleaned_text)
+    cleaned_text = re.sub(r'\/','\n',cleaned_text)
+    cleaned_text = re.sub(r'(早上好|你好)','',cleaned_text)
+    cleaned_text = re.sub(r'[^\u4e00-\u9fff]+','\n',cleaned_text)
+
+    f = open("cleaned.txt", "w+",encoding='utf-8')
+    f.write(cleaned_text)
+    f.close()
+
+def make_anki():
+    print('not_done')
 
 
-file = open("class_notes.txt", "r",encoding='utf-8').read()
+if __name__ == "__main__":
+    clean_notes()
 
+    # if click.confirm('Check cleaned.txt for logic',default=True):
+        make_anki()
