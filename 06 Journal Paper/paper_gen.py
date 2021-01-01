7835c46232254eeb211a10cdb8bb8fbe39e886de
andanao <47489123+andanao@users.noreply.github.com>
2021-01-01 15:28:41 -0500

    moved into class to make cleaner

--- 06 Journal Paper/paper_gen.py
+++ 06 Journal Paper/paper_gen.py
@@ -3,64 +3,71 @@ import webbrowser
 import svgwrite
 from svgwrite import cm, mm, inch  
 
-def note_paper(name):
-    dwg = svgwrite.Drawing(filename=name, debug=True)
+class paper_maker:
+    def __init__(self,name):
+        self.dwg = svgwrite.Drawing(filename=name, debug=True)
 
-    lmarg = 1*cm
-    rmarg = 1*cm
-    tmarg = 1*cm
-    bmarg = 1*cm
+    def note_paper(self):
 
-    pg_corners = [0,0,11,8.5]
-    outlines = pg_outline(dwg,pg_corners)
-    dwg.add(outlines)
+        lmarg = 1*cm
+        rmarg = 1*cm
+        tmarg = 1*cm
+        bmarg = 1*cm
 
-
-    # hlines = dwg.add(dwg.g(id='hlines', stroke='green'))
-    # for y in range(10):
-    #     hlines.add(dwg.line(start=(2*cm, (2+y)*cm), end=(18*cm, (2+y)*cm)))
-    
+        pg_corners = [0,0,11,8.5]
+        self.pg_outline(pg_corners)
+        # dwg.add(outlines)
 
 
+        # hlines = dwg.add(dwg.g(id='hlines', stroke='green'))
+        # for y in range(10):
+        #     hlines.add(dwg.line(start=(2*cm, (2+y)*cm), end=(18*cm, (2+y)*cm)))
+        
 
 
 
-    dwg.save()
 
 
-def pg_outline(drawing, pg_corners):
-    olines = dwg.add(dwg.g(id='olines',
-        stroke='black',
-        stroke_width = '1',
-        stroke_dasharray='2 5',
-        stroke_linejoin="round"
-    ))
-    olines.add(dwg.line(
-        start=(pg_corners[0]*inch, pg_corners[0]*inch), 
-        end=(pg_corners[2]*inch, pg_corners[0]*inch)
+        self.dwg.save()
+
+
+    def pg_outline(self, pg_corners):
+        olines = self.dwg.add(self.dwg.g(id='olines',
+            stroke='black',
+            stroke_width = '1',
+            stroke_dasharray='2 5',
+            stroke_linejoin="round"
         ))
-    olines.add(dwg.line(
-        start=(pg_corners[2]*inch, pg_corners[0]*inch), 
-        end=(pg_corners[2]*inch, pg_corners[3]*inch)
-        ))
-    olines.add(dwg.line(
-        start=(pg_corners[2]*inch, pg_corners[3]*inch), 
-        end=(pg_corners[0]*inch, pg_corners[3]*inch)
-        ))
-    olines.add(dwg.line(
-        start=(pg_corners[0]*inch, pg_corners[3]*inch), 
-        end=(pg_corners[0]*inch, pg_corners[0]*inch)
-        ))
-    olines.add(dwg.line(
-        start=(pg_corners[2]/2*inch, pg_corners[3]*inch), 
-        end=(pg_corners[2]/2*inch, pg_corners[0]*inch)
-        ))
-    
-    return olines
+        olines.add(self.dwg.line(
+            start=(pg_corners[0]*inch, pg_corners[0]*inch), 
+            end=(pg_corners[2]*inch, pg_corners[0]*inch)
+            ))
+        olines.add(self.dwg.line(
+            start=(pg_corners[2]*inch, pg_corners[0]*inch), 
+            end=(pg_corners[2]*inch, pg_corners[3]*inch)
+            ))
+        olines.add(self.dwg.line(
+            start=(pg_corners[2]*inch, pg_corners[3]*inch), 
+            end=(pg_corners[0]*inch, pg_corners[3]*inch)
+            ))
+        olines.add(self.dwg.line(
+            start=(pg_corners[0]*inch, pg_corners[3]*inch), 
+            end=(pg_corners[0]*inch, pg_corners[0]*inch)
+            ))
+        olines.add(self.dwg.line(
+            start=(pg_corners[2]/2*inch, pg_corners[3]*inch), 
+            end=(pg_corners[2]/2*inch, pg_corners[0]*inch)
+            ))
+        
+        # return olines
+
+    # def lines(dw)
 
 if __name__ == '__main__':
-    note_paper('note_paper.svg')
-    webbrowser.open('note_paper.svg')
-# basic_shapes('note_paper.svg')
+    name = 'note_paper_01.svg'
+    maker = paper_maker(name)
+    maker.note_paper()
 
-# open('note_paper.svg')
+    # note_paper('note_paper.svg')
+    webbrowser.open(name)
+
