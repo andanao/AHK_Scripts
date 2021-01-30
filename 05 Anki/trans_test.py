f7322339321d925dedb00e20cef908c9df669074
andanao <47489123+andanao@users.noreply.github.com>
2021-01-30 18:49:06 -0500

    translation class made and tested

--- 05 Anki/trans_test.py
+++ 05 Anki/trans_test.py
@@ -1,49 +1,79 @@
 # %%
-from translate import Translator
-
-to_lang = 'en'
-key1 = '25b27cf625b14347a856c4c75cf34697'
-key2 = '8f77caba0e06480bb2170e1c8415baa1'
-tw = '是在'
-
-trans = Translator(provider='microsoft', to_lang='zh',secret_access_key=key1)
-trans.translate(tw)
-# %%
 import requests, uuid, json
 
-# Add your subscription key and endpoint
-subscription_key = key
-endpoint = "https://api.cognitive.microsofttranslator.com"
+class Translator():
+    '''
+    Using Microsoft translation API to translate
+    Key - API Key
+    src - Source language (default zh-Hans)
+    dest - destination language (default en)
+    '''
+    def __init__(self,key='',src='zh-Hans',dest='en'):
+        if key =='':
+            print('no key defined!')
+        
+        self.subscription_key = key
+        endpoint = "https://api.cognitive.microsofttranslator.com"
+        location = "eastus"
+        path = '/translate'
 
-# Add your location, also known as region. The default is global.
-# This is required if using a Cognitive Services resource.
-# location = "YOUR_RESOURCE_LOCATION"
-location = "eastus"
+        self.constructed_url = endpoint + path
+        self.params = {
+            'api-version': '3.0',
+            'from': src,
+            'to': dest
+        }
+        
+        self.headers = {
+            'Ocp-Apim-Subscription-Key': subscription_key,
+            'Ocp-Apim-Subscription-Region': location,
+            'Content-type': 'application/json',
+            'X-ClientTraceId': str(uuid.uuid4())
+        }
+        # You can pass more than one object in body.
+        self.body = [
+            {'text': 'testing!'}
+        ]
 
-path = '/translate'
-constructed_url = endpoint + path
+    def _make_request(self,body):
+        request = requests.post(
+            self.constructed_url, 
+            params=self.params, 
+            headers=self.headers, 
+            json=body)
+        response = request.json()
+        # print(json.dumps(response, sort_keys=True, ensure_ascii=False,indent=4, separators=(',', ': ')))
+        return response
 
-params = {
-    'api-version': '3.0',
-    'from': 'en',
-    'to': ['zh-CH', 'es']
-}
-constructed_url = endpoint + path
+    def translate(self, word):
+        '''
+        pass word as string or as list
+        '''
+        body = []
+        if isinstance(word,str):
+            body.append({'text':word})
+        if isinstance(word,list):
+            for i in word:
+                body.append({'text':i})
 
-headers = {
-    'Ocp-Apim-Subscription-Key': subscription_key,
-    'Ocp-Apim-Subscription-Region': location,
-    'Content-type': 'application/json',
-    'X-ClientTraceId': str(uuid.uuid4())
-}
+        response =  self._make_request(body)
+        out = []
+        for i in response:
+            out.append(i['translations'][0]['text'])
+        return out
 
-# You can pass more than one object in body.
-body = [{
-    'text': 'testing!'
-}]
+if __name__ == "__main__":
+    to_lang = 'en'
+    key1 = '25b27cf625b14347a856c4c75cf34697'
+    key2 = '8f77caba0e06480bb2170e1c8415baa1'
+    tw = '是在'
+    tl = ['的确','可以吗？']
+    trans = Translator(key=key1)
 
-request = requests.post(constructed_url, params=params, headers=headers, json=body)
-response = request.json()
+    # trans._make_request(body)
 
-print(json.dumps(response, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': ')))
-# %%
+    print(trans.translate(tw))
+    print(trans.translate(tl))
+
+    
+
