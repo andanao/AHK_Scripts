cf32dc04212e1c9c62bf0743e61acbd6531664c6
andanao <47489123+andanao@users.noreply.github.com>
2021-02-08 19:19:53 -0500

    added input logic to process class notes

--- 05 Anki/process_class_notes.py
+++ 05 Anki/process_class_notes.py
@@ -3,7 +3,7 @@ import pandas as pd
 import re
 import click
 
-def clean_notes():
+def clean_notes(input_f = "notes_raw.txt",out_f=''):
     file = open("class_notes.txt", "r",encoding='utf-8').read()
 
     cleaned_text = re.sub(r'(?<=[\u4e00-\u9fff])\d','',file)
@@ -13,14 +13,19 @@ def clean_notes():
     cleaned_text = re.sub(r'(早上好|你好)','',cleaned_text)
     cleaned_text = re.sub(r'[^\u4e00-\u9fff]+','\n',cleaned_text)
 
-    f = open("cleaned.txt", "w+",encoding='utf-8')
-    f.write(cleaned_text)
-    f.close()
+    if out_f=='':
+        f = open(out_f, "w+",encoding='utf-8')
+        f.write(cleaned_text)
+        f.close()
+    else:
+        print("{} cleaned ")
+        return cleaned_text
 
-def make_anki(out_f = "cleaned.txt"):
-    f = open("cleaned.txt", "r",encoding='utf-8').read()
+def make_anki(input_f = "notes_cleaned.txt"):
+    f = open(input_f, "r",encoding='utf-8').read()
     word_list = f.split('\n')
-    print('not_done')
+    print(word_list)
+    print('\n\nINCOMPLETE FN')
 
 
 if __name__ == "__main__":
