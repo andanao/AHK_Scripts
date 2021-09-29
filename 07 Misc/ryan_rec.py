eab678af4d5527fa76b5d8e2f0f512fd33f55100
Adrian Danao-Schroeder <47489123+andanao@users.noreply.github.com>
2021-09-29 15:59:11 -0400

    Update ryan_rec.py

--- 07 Misc/ryan_rec.py
+++ 07 Misc/ryan_rec.py
@@ -6,6 +6,8 @@ class User:
     def __init__(self) -> None:
         self.id = np.random.randint(10000)
         self.articles_read = []
+        # self.likes = np.zeros(10) # 
+        self.likes = {}
 
 
 
@@ -24,8 +26,19 @@ class ArticleDB:
 
     def load_db(self):
         """this should connect to airtable but for the moment it is just gen a random file"""
+        tags = ["ready",
+                "linear",
+                "crop",
+                "host",
+                "unique",
+                "multi",
+                "affect",
+                "floor",
+                "form",
+                "treaty"]
         self.df = pd.DataFrame(np.random.randint(0,2,size=(10, 10)),
-                                index = np.random.randint(0,100000,size=(10)))
+                                index = np.random.randint(0,100000,size=(10)),
+                                columns=tags)
                     
         self.df["num_read"] = np.random.randint(0,1,size=(10))
 
@@ -35,6 +48,10 @@ class ArticleDB:
         """Mark the article as read by the user, increase the popularity of the article"""
         user.articles_read.append(art_id)
         self.df.loc[art_id].num_read += 1
+        for i, val in enumerate(self.df.columns[:-1]): #should get the # or tags, probably should have a better way if more things are going to be added to the DB
+            if self.df.loc[art_id][i]:
+                # print(val)
+                user.likes[val] = 1 #set to one increment later
 
         
 
@@ -49,5 +66,10 @@ class ArticleDB:
 
 
 
-db = ArticleDB()
-df = db.df
+
+
+if __name__ == "__main__":
+    db = ArticleDB()
+    df = db.df
+    user = db.users[0]
+    db.article_read(df.index[0],db.users[0])
