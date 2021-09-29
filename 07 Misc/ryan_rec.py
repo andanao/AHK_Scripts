699ccb07aa677baf9810953735bdeb2aff4a4ae2
Adrian Danao-Schroeder <47489123+andanao@users.noreply.github.com>
2021-09-29 17:28:32 -0400

    Update ryan_rec.py

--- 07 Misc/ryan_rec.py
+++ 07 Misc/ryan_rec.py
@@ -48,17 +48,20 @@ class ArticleDB:
         """Mark the article as read by the user, increase the popularity of the article"""
         user.articles_read.append(art_id)
         self.df.loc[art_id].num_read += 1
-        for i, val in enumerate(self.df.columns[:-1]): #should get the # or tags, probably should have a better way if more things are going to be added to the DB
+        for i, tag in enumerate(self.df.columns[:-1]): #should get the # or tags, probably should have a better way if more things are going to be added to the DB
             if self.df.loc[art_id][i]:
                 # print(val)
-                user.likes[val] = 1 #set to one increment later
+                if tag in user.likes:
+                    user.likes[tag] += 1
+                else:
+                    user.likes[tag] = 1 #set to one increment later
 
         
 
 
-    def suggest_article(self,user):
+    def suggest_articles(self,user):
         """suggest an article to a user"""
-        pass
+        
 
     def suggest_article_new_user(self,user):
         """print out most popular articles"""
@@ -70,6 +73,13 @@ class ArticleDB:
 
 if __name__ == "__main__":
     db = ArticleDB()
+    db.article_read(db.df.index[0],db.users[0])
+    
+    db.article_read(db.df.index[1],db.users[0])
+    db.article_read(db.df.index[0],db.users[1])
+    
+    
+    
+    
     df = db.df
     user = db.users[0]
-    db.article_read(df.index[0],db.users[0])
